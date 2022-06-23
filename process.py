90dfcd5e1414c3840a80da4139baf08911ccb998
Bhuvaneswaran M <99600317+Bhuviabi@users.noreply.github.com>
2022-06-23 21:46:54 +0530

    Update process.py
    comment updates

--- process.py
+++ process.py
@@ -1,13 +1,13 @@
 import numpy as np
 import nltk
-"""once downloded"""#nltk.download('punkt')
+#nltk.download('punkt')
 from nltk.stem.porter import PorterStemmer
 stemmer = PorterStemmer()
 def tokenize(sentence):
-    """split sentence into array of words/tokens a token can be a word or punctuation character, or number"""
+    #split sentence into array of words/tokens a token can be a word or punctuation character, or number
     return nltk.word_tokenize(sentence)
 def stem(word):
-    """stemming = find the root form of the word examples: words = ["organize", "organizes", "organizing"] words = [stem(w) for w in words] -> ["organ", "organ", "organ"]"""
+    #stemming = find the root form of the word examples: words = ["organize", "organizes", "organizing"] words = [stem(w) for w in words] -> ["organ", "organ", "organ"]
     return stemmer.stem(word.lower())
 def bag_of_words(tokenized_sentence, words):
     sentence_words = [stem(word) for word in tokenized_sentence]
